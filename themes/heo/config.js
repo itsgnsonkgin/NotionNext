2c68378223aedab65afd363ffe4178fd2267551b
itsgnsonkgin <127229830+itsgnsonkgin@users.noreply.github.com>
2024-08-22 21:47:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,12 +23,12 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://redditfans.top',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'hot tag', url: '/tag/必看精选' },
-  //HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  //HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'hot tag', url: '/tag' },
+  HEO_HERO_CATEGORY_2: { title: 'category', url: '/category' },
+  HEO_HERO_CATEGORY_3: { title: 'archive', url: '/archive' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: 'TOP',
+  HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
